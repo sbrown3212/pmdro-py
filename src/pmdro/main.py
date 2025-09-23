dc6930234481f9ef27eb4319d0092e3d4ff155c8
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-09-23 16:05:54 -0600

    'start' command now accepts 'focus' and 'break' option parameters.

--- src/pmdro/main.py
+++ src/pmdro/main.py
@@ -7,6 +7,24 @@ def cli():
 
 
 @cli.command("start")
-@click.option("-f", "--focus", type=click.INT)
-def start(focus):
-    click.echo(f"Setting timer for {focus} minutes.")
+@click.option(
+    "-f",
+    "--focus",
+    "focus_duration",
+    type=click.INT,
+    help="Focus timer duration in minutes",
+)
+@click.option(
+    "-b",
+    "--break",
+    "break_duration",
+    type=click.INT,
+    help="Break timer duration in minutes",
+)
+def start(focus_duration, break_duration):
+    click.echo("You ran 'pmdro start'.")
+    if focus_duration is not None:
+        click.echo(f"Setting focus timer for {focus_duration} minutes.")
+
+    if break_duration is not None:
+        click.echo(f"Setting break timer for {break_duration} minutes.")
