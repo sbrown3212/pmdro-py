450fbfe524e370aef7b62e483a5f98f18000168d
Stephen Brown <46618639+sbrown3212@users.noreply.github.com>
2025-10-01 09:30:16 -0600

    Implemented progress bar test.

--- src/pmdro/main.py
+++ src/pmdro/main.py
@@ -1,4 +1,6 @@
 import click
+import threading
+import time
 
 
 @click.group()
@@ -23,10 +25,61 @@ def cli():
     help="Break timer duration in minutes",
     default=5,
 )
+# @click.option("-s", "--split") # combines focus and break timer (syntax might look like 25/5)
 def start(focus_duration, break_duration):
-    click.echo("You ran 'pmdro start'.")
+    focus_seconds = focus_duration * 60
     if focus_duration is not None:
         click.echo(f"Setting focus timer for {focus_duration} minutes.")
 
+    with click.progressbar(range(focus_seconds)) as bar:
+        for _ in bar:
+            time.sleep(1)
+
     if break_duration is not None:
         click.echo(f"Setting break timer for {break_duration} minutes.")
+
+
+# class Timer:
+#     def __init__(self):
+#         self.running = False
+#         self.paused = False
+#         self.thread = None
+#         # self.start_time = None
+#         # self.total_paused_duration = None # or 0
+#         self.end_time = None
+#
+#     def start(self, duration_minutes):
+#         """Start countdown timer"""
+#         if self.running is True:
+#             return False
+#
+#         self.running = True
+#         self.paused = False
+#         self.end_time = time.time() + (duration_minutes * 60)
+#         self.thread = threading.Thread(target=self._countdown_loop)
+#         self.thread.daemon = True
+#         self.thread.start()
+#         return True
+#
+#     def _countdown_loop(self):
+#         """Internal countdown loop"""
+#         while self.running:
+#             if not self.paused:
+#                 current_time = time.time()
+#                 remaining = self.end_time - current_time
+#
+#                 if remaining <= 0:
+#                     print("\nTime's up")
+#                     self.running = False
+#                     break
+#
+#                 min, sec = divmod(int(remaining), 60)
+#                 print(f"\r{min:02d}:{sec:02d}", end="", flush=True)
+#
+#             time.sleep(0.1)
+#
+#     def stop(self):
+#         """Terminate countown timer."""
+#         self.running = False
+#         if self.thread:
+#             self.thread.join()
